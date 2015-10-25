474cbfd6cf240364ec4739b91d21f0a08efa0507
Taylor Fahlman <you@example.com>
2015-10-25 10:14:06 -0700

    Added struct and shell functions

--- linux-yocto-3.14/block/sstf-iosched.c
+++ linux-yocto-3.14/block/sstf-iosched.c
@@ -7,6 +7,63 @@
 #include <linux/module.h>
 #include <linux/slab.h>
 
+struct look_data {
+    struct list_head queue;
+}
+
+static void look_merged requests
+(struct request_queue *q, struct request *rq, struct request *next)
+{
+
+}
+
+static int look_dipatch(struct request_queue *q, int force)
+{
+
+}
+
+static void look_add_request(struct request_queue *q, struct request *rq)
+{
+
+}
+
+static struct request * 
+look_former_request(struct request_queue *q, struct request *rq)
+{
+
+}
+
+static struct request * 
+look_latter_request(struct request_queue *q, struct request *rq)
+{
+
+}
+
+static void look_init_queue(struct request_queue *q, struct elevator_type *e)
+{
+
+}
+
+static void look_exit_queue(struct elevator_queue *e)
+{
+
+}
+
+static struct elevator_type elevator_look = {
+    .ops = {
+        .elevator_merge_req_fn = look_merged_requests,
+        .elevator_dispatch_fn = look_dispatch,
+        .elevator_add_req_fn = look_add_request,
+        .elevator_former_req_fn = look_former_request,
+        .elevator_latter_req_fn = look_latter_request,
+        .elevator_init_fn = look_init_queue,
+        .elevator_exit_fn = look_exit_queue,
+    },
+    .elevator_name = "look",
+    .elevator_owner = THIS_MODULE,
+
+};
+
 static int __init look_init(void)
 {
 	return elv_register(&elevator_look);

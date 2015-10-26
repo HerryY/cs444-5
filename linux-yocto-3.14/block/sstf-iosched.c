16dfafe2b2ddaa55c00dc6f13d1624f5f348abdc
Taylor Fahlman <you@example.com>
2015-10-26 11:15:17 -0700

    Added debugging statements

--- linux-yocto-3.14/block/sstf-iosched.c
+++ linux-yocto-3.14/block/sstf-iosched.c
@@ -39,13 +39,16 @@ static int look_dispatch(struct request_queue *q, int force)
         //Check if the next is the previous, if there one element in the list
         if(prev_req == next_req)
         {
+            printk("One request\n");
             req = prev_req;   
         }
         else
         {
+            printk("Mutliple requests\n");   
             //If direction is going forward
             if(nd->direction == 1)
             {
+                printk("Going forward\n");
                 //If the next request is further forward
                 if(next_req->__sector > nd->head_position)
                 {
@@ -63,6 +66,7 @@ static int look_dispatch(struct request_queue *q, int force)
             //If the direction is backward
             else
             {
+                printk("Going backward\n");
                 //If the next request behind
                 if(prev_req->__sector < nd->head_position)
                 {
@@ -78,16 +82,17 @@ static int look_dispatch(struct request_queue *q, int force)
                 }
             }
         }
-       
+        printk("Executing\n");   
         //Remove it from the queue we just got it from
         list_del_init(&req->queuelist);
         //get the new position for the read head
         nd->head_position = blk_rq_pos(req) + blk_rq_sectors(req);
         //give request to the elevator 
         elv_dispatch_add_tail(q, req);
+        printk("Done Executing\n");
+        printk("Taylor's LOOK accessing %llu\n",(unsigned long long) req->__sector);
         return 1;
 
-        printk("Taylor's LOOK accessing %llu\n",(unsigned long long) req->__sector);
     }   
     return 0;
 }
@@ -97,15 +102,18 @@ static void look_add_request(struct request_queue *q, struct request *rq)
     struct look_data *nd = q->elevator->elevator_data;
     struct request *next_req, *prev_req;
     sector_t next_req_sector, current_req_sector;
-    
+ 
+    printk("Starting add\n");
     //If list is empty
     if(list_empty(&nd->queue))
     {
+        printk("List is empty\n");
         //Just add the request
         list_add(&rq->queuelist, &nd->queue);
     }
     else
     {
+        printk("Finding place for request\n");
         //Find where the request fits into the list of requests
         next_req = list_entry(nd->queue.next, struct request, queuelist);
         prev_req = list_entry(nd->queue.prev, struct request, queuelist);
@@ -123,6 +131,7 @@ static void look_add_request(struct request_queue *q, struct request *rq)
         }
         //Adds the current request between the two nodes
         __list_add(&rq->queuelist, &prev_req->queuelist, &next_req->queuelist);
+        printk("Found it\n");
     }
     printk("Taylor's LOOK adding %llu\n", (unsigned long long) rq->__sector);
 }
@@ -157,7 +166,7 @@ static int look_init_queue(struct request_queue *q, struct elevator_type *e)
     if(!eq)
         return -ENOMEM;
 
-    nd = kmalloc_node(sizeof(nd), GFP_KERNEL, q->node);
+    nd = kmalloc_node(sizeof(*nd), GFP_KERNEL, q->node);
     if(!nd)
     {
         kobject_put(&eq->kobj);
@@ -167,7 +176,6 @@ static int look_init_queue(struct request_queue *q, struct elevator_type *e)
     eq->elevator_data = nd;
 
     INIT_LIST_HEAD(&nd->queue);
-    nd->head_position = 0;
     nd->direction = 1;
     spin_lock_irq(q->queue_lock);
     q->elevator = eq;
@@ -179,6 +187,7 @@ static void look_exit_queue(struct elevator_queue *e)
 {
     struct look_data *nd = e->elevator_data;
 
+    BUG_ON(!list_empty(&nd->queue));
     kfree(nd);
 }
 

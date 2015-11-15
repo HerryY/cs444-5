41904d4f66fa1a22a5d3c9ad3738074e08ecd403
Taylor Fahlman <you@example.com>
2015-11-15 15:28:56 -0800

    Added functions for pop, push and line and chair structs

--- concurrency/assignment4/assignment4part2.c
+++ concurrency/assignment4/assignment4part2.c
@@ -16,8 +16,27 @@ void barber(void *);
 void customer(void *);
 void get_hair_cut(void);
 void cut_hair(void);
+void line_pop(void);
+void line_push(void);
+
+struct chair {
+
+    int taken;
+    struct chair *next;
+};
+
+struct line {
+
+    int number_of_customers;
+    int chairs;
+    struct chair *current;
+    struct chair *head;
+};
+
+struct line global_queue;
 
 void sig_catch(int sig){
+    
     printf("Catching signal %d\n", sig);
     kill(0,sig);
     exit(0);
@@ -25,6 +44,8 @@ void sig_catch(int sig){
 
 void barber(void *queue)
 {
+    while(
+
 
 }
 
